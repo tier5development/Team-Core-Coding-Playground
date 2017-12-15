f2c6a21edde985c81b86821e87ed24123215c191
Tier5 <work@tier5.us>
2017-12-15 19:34:53 +0530

    some changes are made

--- firstphp.php
+++ firstphp.php
@@ -1,14 +1,5 @@
 <?php
-
+echo "Hi";
 echo "This is Signin Page";
-<div class="container">
-    <label><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <button type="submit">Login</button>
-    <input type="checkbox" checked="checked"> Remember me
-  </div>
+echo "This is signin Page";
 ?>
