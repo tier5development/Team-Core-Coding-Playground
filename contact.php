0df55fd6e31216cad9df7f8707560b41ac92649b
Tier5 <work@tier5.us>
2018-02-28 12:56:10 +0530

    wish message

---
+++ contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Form</title>
+<link rel="stylesheet" href="Contact.css">
+</head>
+<body>
+Some changes have been made in a file
+<h1>Elegant Contact Form</h1>
+<form class="cf">
+  <div class="half left cf">
+    <input type="text" id="input-name" placeholder="Name">
+    <input type="email" id="input-email" placeholder="Email address">
+    <input type="text" id="input-subject" placeholder="Subject">
+  </div>
+  <div class="half right cf">
+    <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+  </div>  
+  <input type="submit" value="Submit" id="input-submit">
+</form>
+
+</body>
+</html>
