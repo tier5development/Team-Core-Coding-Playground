b2b01f60b68b8dc9dd1eeccf3c18feef402faed0
Tier5LLC <github@tier5.us>
2018-03-09 17:20:44 +0530

    This is the first version to add two numbers.

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+        int a , b , sum;
+        printf("\nEnter the 1st number\n");
+        scanf("%d",&a);
+        printf("\nEnter the 2nd number\n");
+        scanf("%d",&b);
+        sum = a + b;
+        printf("\nThe sum of %d and %d is %d\n",a,b,sum);
+}
