97b7cbdf147831105e62639c2e476735dadc8447
Tier5LLC <github@tier5.us>
2018-03-09 17:40:24 +0530

    Added comments in the code.

--- add.c
+++ add.c
@@ -2,10 +2,11 @@
 void main()
 {
         int a , b , sum;
-        printf("\nEnter the 1st number\n");
-        scanf("%d",&a);
-        printf("\nEnter the 2nd number\n");
-        scanf("%d",&b);
-        sum = a + b;
-        printf("\nThe sum of %d and %d is %d\n",a,b,sum);
+        printf("\nEnter the 1st number\n"); // 1st number
+        scanf("%d",&a); // a will store the first number
+        printf("\nEnter the 2nd number\n"); // 2nd number
+        scanf("%d",&b); // b will store the 2nd number
+        sum = a + b; // sum will the addition of the value present in a and b
+        printf("\nThe sum of %d and %d is %d\n",a,b,sum); // print the value of sum
+
 }
