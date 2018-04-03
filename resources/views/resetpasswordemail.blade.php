800fb65d58725fab4dbc95b4e9a8393396387730
Tier5LLC <github@tier5.us>
2018-04-03 12:09:20 +0530

    Edition in link of change password v1.0.1

--- resources/views/resetpasswordemail.blade.php
+++ resources/views/resetpasswordemail.blade.php
@@ -5,5 +5,5 @@
 <p>Some text</p>
 
 <p>Some more text</p> <br>
-<a href="{{route('project.reset_password_view',['token' => $token])}}"> Click hare to redirect</a>
+<a href="{{route('project.reset_password_view',['token' => $token])}}"> Click here to change the password of Team Core</a>
 <br> <h4> Regards</h4><br><h5>dev.tarun.hybrid.core</h5>
