55dfc307427a3955eb9cac1df031c3300c3d0c54
root <tier5@github.com>
2018-04-04 18:51:27 +0530

    Validation added

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -13,41 +13,58 @@
     <label for="name">Name</label>
     <input type="text" class="form-control" name="name" aria-describedby="name" placeholder="Enter name" required>
     <br>
-    @if($errors->has('name'))
-      {{$errors->first('name')}}
-    @endif
+      
+      @if($errors->has('name'))
+      <div class="alert alert-danger">
+        {{$errors->first('name')}}
+         </div>
+      @endif
+     
   </div>
   <div class="form-group">
     <label for="name">Last name</label>
     <input type="text" class="form-control" name="lname" aria-describedby="lname" placeholder="Enter last name" required>
      <br>
-    @if($errors->has('lname'))
-      {{$errors->first('lname')}}
-    @endif
+       
+      @if($errors->has('lname'))
+      <div class="alert alert-danger">
+        {{$errors->first('lname')}}
+         </div>
+      @endif
+      
   </div>
   <div class="form-group">
     <label for="email">Email id</label>
     <input type="text" class="form-control" name="email" aria-describedby="email" placeholder="Enter email" required>
       <br>
-    @if($errors->has('email'))
-      {{$errors->first('email')}}
-    @endif
+        
+      @if($errors->has('email'))
+      <div class="alert alert-danger">
+        {{$errors->first('email')}}
+        </div>
+      @endif
+      
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" name="password" placeholder="Password" required>
     <br>
-     @if($errors->has('password'))
-      {{$errors->first('password')}}
-    @endif
+      
+       @if($errors->has('password'))
+       <div class="alert alert-danger">
+        {{$errors->first('password')}}
+      </div>
+       @endif
   </div>
   <div class="form-group">
     <label for="Passwordc">Re type Password</label>
     <input type="password" class="form-control" name="passwordc" id="passwordc"  placeholder="Conform Password"  required>
     <br>
-    @if($errors->has('passwordc'))
-      {{$errors->first('passwordc')}}
-    @endif
+      @if($errors->has('passwordc'))
+      <div class="alert alert-danger">
+        {{$errors->first('passwordc')}}
+      </div>
+      @endif
   </div>
 
   <button type="submit" class="btn btn-primary">Submit</button>
